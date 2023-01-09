2579de1c0545fde540f0a90057bf82018012fcef
roshan786143 <roshanbabu.kng@gmail.com>
2023-01-09 15:21:45 +0530

    storing data on sql database

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import router from "./routes/router.js";
 import path, {dirname} from 'path';
+import {db} from './models/data.js';
+
 
 
 const myFilePath = path.resolve(dirname('./'), "views", "index.html");
@@ -10,8 +12,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', function(req, res) {
+app.get('/', (req, res)=> {
   res.sendFile(myFilePath);
+});
+
+app.post('/',(req, res) => {
+  const q = "INSERT INTO user(`name`, `email`,`password`) VALUES (?)";
+
+  const values = [
+    req.body.name,
+    req.body.email,
+    req.body.password
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
 });
 
 app.use('/expenses',router);
