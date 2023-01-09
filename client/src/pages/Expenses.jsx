9a80a33e04993f99d16dc62c9803659f02c3f48e
roshan786143 <roshanbabu.kng@gmail.com>
2023-01-09 19:01:46 +0530

    forgot password

--- client/src/pages/Expenses.jsx
+++ client/src/pages/Expenses.jsx
@@ -21,6 +21,48 @@ const Expenses = () => {
 
   console.log(expenses);
 
+  return (
+    <div className="form">
+      <h1>Update the Expense</h1>
+      <input
+        type="text"
+        placeholder="Expense For :"
+        name="title"
+        onChange={handleChange}
+      />
+      {/* <textarea
+        rows={3}
+        type="text"
+        placeholder="Expense desc"
+        name="desc"
+        onChange={handleChange}
+      /> */}
+      <input
+        type="number"
+        placeholder="Expense amount"
+        name="price"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        placeholder="Expense description"
+        name="desc"
+        onChange={handleChange}
+      />
+      {/* <input
+        type="text"
+        placeholder="Book cover"
+        name="cover"
+        onChange={handleChange}
+      /> */}
+      <button onClick={handleClick}>Update</button>
+      {error && "Something went wrong!"}
+      <Link to="/">See all expenses</Link>
+    </div>
+  );
+};
+
+
   const handleDelete = async (id) => {
     try {
       await axios.delete(`http://localhost:8800/expenses/${id}`);
@@ -37,7 +79,7 @@ const Expenses = () => {
         {expenses.map((expense) => (
           <div key={expense.id} className="expense">
             {/* <img src={book.cover} alt="" /> */}
-            <h2>{expense.title}</h2>
+            <h2 style={{backgroundColor:'#ADA2FF',borderRadius:'2px',color:'red'}}>{expense.title}</h2>
             <p>{expense.desc}</p>
             <span>₹{expense.price}/-</span>
             <button className="delete" onClick={() => handleDelete(expense.id)}>Delete</button>
