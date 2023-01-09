35296f111895430241a14bf933d924fcc01a988f
roshan786143 <roshanbabu.kng@gmail.com>
2023-01-09 18:59:43 +0530

    feature update

--- client/src/pages/Update.jsx
+++ client/src/pages/Update.jsx
@@ -32,6 +32,50 @@ const Update = () => {
     }
   };
 
+  export const postExpenses = (req, res) => {
+    const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
+  
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.price,
+      req.body.cover,
+    ];
+  
+    db.query(q, [values], (err, data) => {
+      if (err) return res.send(err);
+      return res.json(data);
+    });
+  };
+
+export  const deleteExpenses = (req, res) => {
+    const expenseId = req.params.id;
+    const q = " DELETE FROM books WHERE id = ? ";
+  
+    db.query(q, [expenseId], (err, data) => {
+      if (err) return res.send(err);
+      return res.json(data);
+    });
+  };
+
+export  const updateExpenses = (req, res) => {
+    const expenseId = req.params.id;
+    const q = "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ?";
+  
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.price,
+      req.body.cover,
+    ];
+  
+    db.query(q, [...values,expenseId], (err, data) => {
+      if (err) return res.send(err);
+      return res.json(data);
+    });
+  };
+
+
   return (
     <div className="form">
       <h1>Update the Expense</h1>
