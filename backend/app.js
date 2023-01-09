09d16bd25b9dff043d1f81472b449cb633c8c6e4
roshan786143 <roshanbabu.kng@gmail.com>
2023-01-09 19:18:17 +0530

    Report Generation Backend

--- backend/app.js
+++ backend/app.js
@@ -36,17 +36,29 @@ document.getElementById('rzp-button1').onclick = async function (e) {
      "theme": {
       "color": "#3399cc"
      },
-     // This handler function will handle the success payment
      "handler": function (response) {
-         console.log(response);
-         axios.post('http://localhost:3000/purchase/updatetransactionstatus',{
-             order_id: options.order_id,
-             payment_id: response.razorpay_payment_id,
-         }, { headers: {"Authorization" : token} }).then(() => {
-             alert('You are a Premium User Now')
-         }).catch(() => {
-             alert('Something went wrong. Try Again!!!')
-         })
-     },
-  };
+        console.log(response);
+        axios.post('http://localhost:3000/purchase/updatetransactionstatus',{
+            order_id: options.order_id,
+            payment_id: response.razorpay_payment_id,
+        }, { headers: {"Authorization" : token} }).then(() => {
+            alert('You are a Premium User Now')
+        }).catch(() => {
+            alert('Something went wrong. Try Again!!!')
+        })
+    },
+ };
+ const rzp1 = new Razorpay(options);
+ rzp1.open();
+ e.preventDefault();
+
+ rzp1.on('payment.failed', function (response){
+ alert(response.error.code);
+ alert(response.error.description);
+ alert(response.error.source);
+ alert(response.error.step);
+ alert(response.error.reason);
+ alert(response.error.metadata.order_id);
+ alert(response.error.metadata.payment_id);
+});
 }
